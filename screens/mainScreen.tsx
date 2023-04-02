2d229835b8c6f7359b5f9fa8b62eaf6e165ebff2
ArlatPS <przemekx10@gmail.com>
2023-04-02 16:31:50 +0200

    webview and logo

--- screens/mainScreen.tsx
+++ screens/mainScreen.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import BestDeals from "../components/main/bestDeals";
+import WebView from "react-native-webview";
 
 export default function MainScreen() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>GG</Text>
+      <WebView
+        source={{ uri: "https://embed.lottiefiles.com/animation/78811" }}
+        style={{ flex: 1 }}
+        originWhitelist={["*"]}
+      />
+      <Image
+        source={require("../assets/gdclog.png")}
+        style={{ height: 50, aspectRatio: 1 }}
+      />
       <BestDeals />
     </View>
   );
